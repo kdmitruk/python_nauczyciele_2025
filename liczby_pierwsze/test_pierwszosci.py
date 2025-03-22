300c36736b593c1ea5062436bdfcd7f4926e283f
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-03-22 13:03:32 +0100

    test pierwszosci, wersja 2

--- liczby_pierwsze/test_pierwszosci.py
+++ liczby_pierwsze/test_pierwszosci.py
@@ -2,7 +2,7 @@ def czy_pierwsza(liczba : int):
     if liczba < 2:
         return False
 
-    for i in range(2, int(liczba ** 0.5)):
+    for i in range(2, int(liczba ** 0.5) + 1):
         if liczba % i == 0:
             return False
     return True
