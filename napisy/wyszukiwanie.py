b77add0dbf09dc9caa1cada0e1685776fe8b4ae0
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-04-05 14:27:53 +0200

    wyszukiwanie naiwne, rozpisane

--- napisy/wyszukiwanie.py
+++ napisy/wyszukiwanie.py
@@ -4,7 +4,18 @@ def wyszukaj_naiwnie(sprawdzany, szukany):
             return i
     return None
 
+def wyszukaj_naiwnie_v2(sprawdzany, szukany):
+    for i in range(len(sprawdzany) - len(szukany) + 1):
+        znaleziono = True
+        for j in range(len(szukany)):
+            if not sprawdzany[i+j] == szukany[j]:
+                znaleziono = False
+                break
+        if znaleziono:
+            return i
+    return None
+
 if __name__ == '__main__':
     sprawdzany = "Ala ma tako"
     szukany = "ko"
-    print(wyszukaj_naiwnie(sprawdzany, szukany))
+    print(wyszukaj_naiwnie_v2(sprawdzany, szukany))
