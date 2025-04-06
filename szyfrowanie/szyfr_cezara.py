dd0924cc663e18e28832a3225fd42a5a8220c8dd
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-04-06 13:13:19 +0200

    szyfr cezara - dowolny alfabet

--- szyfrowanie/szyfr_cezara.py
+++ szyfrowanie/szyfr_cezara.py
@@ -1,15 +1,16 @@
-# alfabet = ""
-# for i in range(ord('a'), ord('z') + 1):
-#     alfabet+=(chr(i))
+import random
 
-alfabet = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+alfabet = [chr(i) for i in range(ord('a'), ord('z') + 1)] + list("ąćęłóźż")
+random.shuffle(alfabet)
+print(alfabet)
 
 def szyfruj(napis, klucz):
     szyfr = [alfabet[(i + klucz) % len(alfabet)] for i in range(len(alfabet))]
+    print(szyfr)
     wynik = ""
     for znak in napis:
-        if ord('a') <= ord(znak) <= ord('z'): #znak.islower():
-            indeks = ord(znak) - ord('a')
+        if znak in alfabet:
+            indeks = alfabet.index(znak)
             zaszyfrowany_znak = szyfr[indeks]
             wynik += zaszyfrowany_znak
         else:
