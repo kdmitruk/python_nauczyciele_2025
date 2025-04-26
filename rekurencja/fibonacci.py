3afbad1bf01d51a211bd44e565bea5e84cbc3f47
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-04-26 14:16:08 +0200

    fibonacci rekurencja ogonowa

--- rekurencja/fibonacci.py
+++ rekurencja/fibonacci.py
@@ -23,6 +23,14 @@ def f_iteracyjny_skrocony(n):
        fn_2, fn_1 = fn_1, fn_2 + fn_1
     return fn_1
 
+def f_rekurencja_ogonowa(n, fn_2 = 0 , fn_1 = 1):
+    if n == 0:
+        return fn_2
+    elif n == 1:
+        return fn_1
+    else:
+        return f_rekurencja_ogonowa(n-1, fn_1, fn_2 + fn_1)
+
 if __name__ == '__main__':
-    print(f_iteracyjny_skrocony(15))
+    print(f_rekurencja_ogonowa(50))
 
