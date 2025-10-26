6e97fec300bee4d5d5725130f48b3e6e70c71c7c
kdmitruk <kdmitruk@gmail.com>
2025-10-26 12:28:11 +0100

    zad3, brak ostatniego porównania

--- zlozonosc/z3.py
+++ zlozonosc/z3.py
@@ -9,12 +9,12 @@ def znajdz_indeksy(dane, suma):
 # O(N) = N^2
 
 def znajdz_indeksy_v2(dane, suma):
-    for i in range(len(dane)):  # N
-        for j in range(i+1, len(dane)): # N(N+1)/2 = (N^2+N)/2 ---> N^2
+    for i in range(len(dane)-1):  # N-1
+        for j in range(i+1, len(dane)): # (N-1)N/2 = (N^2-N)/2 ---> N^2
             if dane[i] + dane[j] == suma:
                 return i, j
 
-# T(N) = N(N+1)/2
+# T(N) = N(N-1)/2
 # O(N) = N^2
 
 dane = [3, 7, 9, 12, 19, 30, 41, 56, 71]
