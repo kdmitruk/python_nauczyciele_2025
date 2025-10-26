d7e4804cbe474115802520726dc96dfb52d6ed3a
kdmitruk <kdmitruk@gmail.com>
2025-10-26 10:04:09 +0100

    brute force z printem

--- zlozonosc/wyszukiwanie.py
+++ zlozonosc/wyszukiwanie.py
@@ -1,9 +1,12 @@
 def brute_force(dane, szukana):
     for i in range(len(dane)):
         if dane[i] == szukana:
+            print("znalazłem")
             return i
+        else:
+            print("nie znalazłem")
     return None
 
 if __name__ == '__main__':
     dane = [3, 7, 9, 12, 19, 30, 41, 56]
-    print(brute_force(dane, 12))
+    print(brute_force(dane, 112))
