2ed6de054443b76e09ced6e07ff6b14be031f6f9
kdmitruk <kdmitruk@gmail.com>
2025-10-26 12:29:55 +0100

    zad4 init

---
+++ zlozonosc/z4.py
@@ -0,0 +1,21 @@
+def fun1(arr1, arr2):
+    for i in range(0, len(arr1)):
+        print(arr1[i])
+    for j in range(0, len(arr2)):
+        print(arr2[j])
+
+def fun2(arr1, arr2):
+    for i in range(0, len(arr1)):
+        for j in range(0, len(arr2)):
+            print(arr1[i] + arr2[j])
+
+def fun3(arr1, arr2):
+    for i in range(0, len(arr1)):
+        for j in range(0, len(arr2)):
+            print(arr1[i] + arr2[j])
+
+
+def fun4(arr1, arr2):
+    for i in range(0, len(arr1)):
+        for j in range(0, len(arr2)):
+            print(arr1[i] + arr2[j])
