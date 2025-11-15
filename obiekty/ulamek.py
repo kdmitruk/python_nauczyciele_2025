e498d6cc1dce3170b559effed0d478fcc01496d9
kdmitruk <kdmitruk@gmail.com>
2025-11-15 15:13:19 +0100

    obiekty - z7

--- obiekty/ulamek.py
+++ obiekty/ulamek.py
@@ -1,3 +1,5 @@
+from math import gcd
+
 class Ulamek:
     def __init__(self, licznik, mianownik):
         self.licznik = licznik
@@ -9,13 +11,25 @@ class Ulamek:
     def __str__(self):
         return f"{self.licznik}/{self.mianownik}"
 
+    def __float__(self):
+        return self.licznik / self.mianownik
+
+    def zredukuj(self):
+        nwd = gcd(self.licznik, self.mianownik)
+        self.licznik //= nwd
+        self.mianownik //= nwd
+
 def porownaj(a: Ulamek, b: Ulamek):
-    wartosc_a = a.licznik / a.mianownik
-    wartosc_b = b.licznik / b.mianownik
+    wartosc_a = float(a)
+    wartosc_b = float(b)
     if wartosc_a < wartosc_b: return -1
     elif wartosc_a == wartosc_b: return 0
     else: return 1
 
 if __name__ == '__main__':
-    u = Ulamek(8, 4)
-    print(u.__str__())
+    u = Ulamek(12, 36)
+    u.zredukuj()
+    print(u)
+
+    #print(float(u))
+    #print(porownaj(Ulamek(2, 3), Ulamek(3,  4)))
