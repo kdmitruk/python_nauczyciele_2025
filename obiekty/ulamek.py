18bc8b08381661a367f4c9a420922acd51e25357
kdmitruk <kdmitruk@gmail.com>
2025-11-15 14:41:00 +0100

    obiekty - z4

--- obiekty/ulamek.py
+++ obiekty/ulamek.py
@@ -6,6 +6,13 @@ class Ulamek:
     def czy_calkowita(self):
         return self.licznik % self.mianownik == 0
 
+def porownaj(a: Ulamek, b: Ulamek):
+    wartosc_a = a.licznik / a.mianownik
+    wartosc_b = b.licznik / b.mianownik
+    if wartosc_a < wartosc_b: return -1
+    elif wartosc_a == wartosc_b: return 0
+    else: return 1
+
 if __name__ == '__main__':
     u = Ulamek(8, 4)
-    print(u.czy_calkowita())
+    print(porownaj(Ulamek(3,  4), Ulamek(2, 3)))
